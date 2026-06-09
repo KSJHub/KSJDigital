1d21412ba8b17e169eb12b87dd1a71b793ee6644
KSJ <83096833+KSJDiamondGaming@users.noreply.github.com>
2026-06-09 03:33:43 +0100

    Clean Sanity client formatting

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-   projectId: '4ph8qwbg',
+  projectId: '4ph8qwbg',
   dataset: 'production',
   apiVersion: '2026-04-30',
   useCdn: true,
