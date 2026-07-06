2bb38a9a3b76acc347b9f114db3dbad251e91b1e
KSJ Hub <sycomorgan@gmail.com>
2026-07-06 08:24:51 +0100

    Import CMS engine styles

--- src/index.jsx
+++ src/index.jsx
@@ -19,6 +19,7 @@ import './styles/brand-manager.css'
 import './styles/pipeline.css'
 import './styles/website-actions.css'
 import './styles/page-builder.css'
+import './styles/site-engine.css'
 import './styles/consistency.css'
 
 createRoot(document.getElementById('root')).render(<App />)
