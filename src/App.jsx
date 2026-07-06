6b5434c6c73aed31967389a7762b4c210a7d0c43
KSJ Hub <sycomorgan@gmail.com>
2026-07-06 03:06:07 +0100

    Route owner support and branded client website page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { AccessDenied } from './components/UI.jsx'
 import { canAccessOwner, getAccountFromPath } from './services/auth.js'
 import { LoginPage } from './pages/LoginPage.jsx'
+import { OwnerSupportPage } from './pages/OwnerSupportPage.jsx'
 import { OwnerWebsitesPage } from './pages/OwnerWebsitesPage.jsx'
-import { ClientsPage, DashboardPage, EditorPage, MediaPage, PublishPage, SettingsPage, SupportPage, WebsitePage } from './pages/Pages.jsx'
+import { ClientWebsitePage } from './pages/ClientWebsitePage.jsx'
+import { ClientsPage, DashboardPage, EditorPage, MediaPage, PublishPage, SettingsPage, SupportPage } from './pages/Pages.jsx'
 
 function route() {
   return location.pathname.replace(/\/$/, '') || '/'
@@ -12,13 +14,14 @@ function Workspace({ client = false, type }) {
   if (!client && type === 'websites') return <OwnerWebsitesPage />
   if (!client && type === 'clients') return <ClientsPage />
   if (!client && type === 'publish-requests') return <PublishPage />
+  if (!client && type === 'support') return <OwnerSupportPage />
   if (client && type === 'publish') return <PublishPage client />
-  if (client && type === 'website') return <WebsitePage />
+  if (client && type === 'website') return <ClientWebsitePage />
   if (type === 'editor') return <EditorPage client={client} />
   if (type === 'media') return <MediaPage client={client} />
   if (type === 'support') return <SupportPage client={client} />
   if (type === 'settings') return <SettingsPage client={client} />
-  return client ? <WebsitePage /> : <OwnerWebsitesPage />
+  return client ? <ClientWebsitePage /> : <OwnerWebsitesPage />
 }
 
 export default function App() {
