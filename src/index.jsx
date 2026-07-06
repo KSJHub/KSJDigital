29d625751ef8f2e787ac2cad6e2bffc34d4b48f3
KSJ Hub <sycomorgan@gmail.com>
2026-07-06 01:50:18 +0100

    Load account management styles

--- src/index.jsx
+++ src/index.jsx
@@ -13,5 +13,6 @@ import './styles/permissions.css'
 import './styles/content-model.css'
 import './styles/publish-review.css'
 import './styles/website-manager.css'
+import './styles/accounts.css'
 import './styles/consistency.css'
 createRoot(document.getElementById('root')).render(<App />)
