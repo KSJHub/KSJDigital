18e1d1b3e93c128b83983c9641b9f5080dfb713d
KSJ Hub <sycomorgan@gmail.com>
2026-07-09 02:13:14 +0100

    Stop caching synced websites in localStorage

--- src/services/serverSync.js
+++ src/services/serverSync.js
@@ -1,18 +1,11 @@
 import { api } from './api.js'
 
-const WEBSITE_KEY = 'ksjDigitalWebsites'
-
 export async function syncWebsitesFromServer() {
   const websites = await api.getWebsites()
-  localStorage.setItem(WEBSITE_KEY, JSON.stringify(websites))
   window.dispatchEvent(new CustomEvent('ksj-websites-updated', { detail: websites }))
   return websites
 }
 
 export function getCachedWebsites() {
-  try {
-    return JSON.parse(localStorage.getItem(WEBSITE_KEY) || '[]')
-  } catch {
-    return []
-  }
+  return []
 }
