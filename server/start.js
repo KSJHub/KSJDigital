cc092a2db754e85861fc353b28ba72987098d176
KSJ Hub <sycomorgan@gmail.com>
2026-07-10 06:31:32 +0100

    Mount PayPal checkout router

--- server/start.js
+++ server/start.js
@@ -1,18 +1,20 @@
 import express from 'express'
 import { createOrdersRouter } from './ordersRouter.js'
+import { createPayPalRouter } from './paypalCheckout.js'
 import { createStripeRouter } from './stripeCheckout.js'
 
 const originalUse = express.application.use
 let useCalls = 0
-let stripeMounted = false
+let checkoutMounted = false
 let ordersMounted = false
 
 express.application.use = function patchedUse(...args) {
   useCalls += 1
 
-  if (!stripeMounted && useCalls === 2) {
-    stripeMounted = true
+  if (!checkoutMounted && useCalls === 2) {
+    checkoutMounted = true
     originalUse.call(this, '/api/checkout/stripe', createStripeRouter())
+    originalUse.call(this, '/api/checkout/paypal', createPayPalRouter())
   }
 
   const result = originalUse.apply(this, args)
