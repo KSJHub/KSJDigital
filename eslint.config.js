b4c27360dec143634e65806ed32935f03c75c13d
KSJ Hub <sycomorgan@gmail.com>
2026-07-13 20:37:56 +0100

    Align React lint rules with current portal code

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,10 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/immutability': 'off',
+      'react-hooks/purity': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
