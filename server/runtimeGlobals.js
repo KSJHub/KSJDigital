523dd4fa08c1321b5d58e10e8e46bad7efc128c2
KSJ Hub <sycomorgan@gmail.com>
2026-07-14 18:15:30 +0100

    Run approval queue cleanup at startup

--- server/runtimeGlobals.js
+++ server/runtimeGlobals.js
@@ -1,7 +1,9 @@
 import fs from 'node:fs/promises'
 import { migrateActiveSites } from './activeSitesMigration.js'
+import { collapseDuplicateApprovalRequests } from './approvalQueueCleanup.js'
 import './identityAccessRuntime.js'
 import './publishingRuntime.js'
 
 globalThis.fs = fs
 await migrateActiveSites()
+await collapseDuplicateApprovalRequests()
